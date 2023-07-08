c98332b6638b44c737c4a4acdbbf6582361716fd
gabri <gabri@DESKTOP-I4U4N50>
2023-07-07 21:28:03 -0300

    Adicionar arquivos de projeto.

---
+++ ATIV7-PROGRAMA-SENAC/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] alunos = new string[5]; // Cria um array para armazenar os nomes dos alunos
+
+        // Preenche o array com os nomes dos alunos
+        for (int i = 0; i < alunos.Length; i++)
+        {
+            Console.WriteLine($"Digite o nome do aluno {i + 1}:");
+            alunos[i] = Console.ReadLine();
+        }
+
+        // Exibe os nomes dos alunos usando o foreach
+        Console.WriteLine("Nomes dos alunos:");
+        foreach (string aluno in alunos)
+        {
+            Console.WriteLine(aluno);
+        }
+    }
+}
